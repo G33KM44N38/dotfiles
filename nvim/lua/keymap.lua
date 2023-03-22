4d2daed025080448706bd037041144b3373363e4
Kylian Mayanga-Fournier <mayanga.kylian@gmail.com>
2023-03-22 22:43:30 +0100

    delete all the useless thing

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -39,43 +39,27 @@ keymap("n", "<leader>gs", ":G<CR>", opts)
 keymap("n", "<leader>gc", ":Git commit<CR>", opts)
 keymap("n", "<leader>gp", ":Git push<CR>", opts)
 keymap("n", "<leader>gb", ":Git checkout", opts)
-keymap("n", "<leader>g1", ":diffget //2<CR> ", opts)
-keymap("n", "<leader>g2", ":diffget //3<CR> ", opts)
-
--- mapping find replace word undercursor in all buffer
-keymap("n", "<Leader>sed", ":bufdo %s/<<C-r><C-w>>//g<Left><Left> | update", opts)
-keymap("n", "<Leader>se", ":s/<<C-r><C-w>>//g<Left><Left>", opts)
 
 -- open term
 keymap("n", "<leader>tt", ":ToggleTerm direction=float fish<CR>", opts)
 keymap("n", "<leader>tv", ":ToggleTerm direction=vertical size=100 fish<CR>", opts)
 keymap("n", "<leader>th", ":ToggleTerm direction=horizontal size=100 fish<CR>", opts)
 
--- mark
-keymap("n", "<leader><leader>1", ":mark A <CR>", opts)
-keymap("n", "<leader><leader>2", ":mark B <CR>", opts)
-keymap("n", "<leader><leader>3", ":mark C <CR>", opts)
-keymap("n", "<leader>1", "'A", opts)
-keymap("n", "<leader>2", "'B", opts)
-keymap("n", "<leader>3", "'C", opts)
-
 -- mapping Open Buffer fzf telescope
 keymap("n", "<leader>bd", ":bd<CR>", opts)
 keymap("n", "<leader>bb", ":lua require'telescope.builtin'.buffers()<CR>", opts)
 
 -- keymap("n", "<leader>t", ":! ctags <CR> :lua require('telescope.builtin').tags()<CR>", opts)
-keymap("n", "<leader>bf", ":lua require('telescope.builtin').find_files()<CR>", opts)
+keymap("n", "bf", ":lua require('telescope.builtin').find_files()<CR>", opts)
 keymap("n", "<leader>dot", ":lua require('rc_telescope').search_dotfiles()<CR>", opts)
 keymap("n", "<leader>conf", ":lua require('rc_telescope').config()<CR>", opts)
 keymap("n", "?", ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", opts)
-keymap("n", "<leader>br", ":lua require('telescope.builtin').live_grep()<CR>", opts)
+keymap("n", "br", ":lua require('telescope.builtin').live_grep()<CR>", opts)
 keymap("n", "<leader>bq", ":lua require('telescope.builtin').quickfix()<CR>", opts)
-keymap("n", "<leader>bg", ":lua require('telescope.builtin').git_files()<CR>", opts)
+keymap("n", "gf", ":lua require('telescope.builtin').git_files()<CR>", opts)
 keymap("n", "<leader>xx", ":lua require('telescope.builtin').diagnostics()<CR>", opts)
-keymap("n", "<leader>gt", ":lua require('telescope.builtin').git_status()<CR>", opts)
 
 keymap("n", "<leader>km", ":lua require('telescope.builtin').keymaps()<CR>", opts)
-keymap("n", "<leader>man", ":lua require('telescope.builtin').man_pcages()<CR>", opts)
 keymap("n", "<leader>old", ":lua require('telescope.builtin').oldfiles()<CR>", opts)
 keymap("n", "<leader>reset", ":LspRestart<CR>", opts)
 keymap("n", "<leader>cheat", ":Cheat<CR>", opts)
@@ -102,7 +86,6 @@ vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
-vim.keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { silent = true })
 
 require("nvim-dap-virtual-text").setup()
 require('dap-go').setup()
@@ -114,7 +97,6 @@ vim.keymap.set("n", "<leader>T", ":TestFile<CR>", { silent = true })
 vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", { silent = true })
 vim.keymap.set("n", "<leader>l", ":TestLast<CR>", { silent = true })
 vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { silent = true })
-
 
 -- Visual Block --
 -- Move text up and down
