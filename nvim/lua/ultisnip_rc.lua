d5bc16a509e85ced05bc59f24202345485ba2b57
Kylian Mayanga-Fournier <mayanga.kylian@gmail.com>
2023-03-25 00:45:45 +0100

    configure my snippet windows

--- nvim/lua/ultisnip_rc.lua
+++ nvim/lua/ultisnip_rc.lua
@@ -1,2 +1,5 @@
+vim.g.UltiSnipsEditSplit = "vertical"
+vim.g.UltiSnipsJumpForwardTrigger="<c-b>"
+vim.g.UltiSnipsJumpBackwardTrigger="<c-z>"
 vim.g.UltiSnipsSnippetDirectory = os.getenv("HOME") .. "/.config/nvim/my_snip"
 
