4f1a92dbe28b3c0ea62b1e7401ec6e2bb443619a
Kylian Mayanga-Fournier <mayanga.kylian@gmail.com>
2023-04-19 22:48:51 +0200

    change the config to check if plugin is good before continuing calling other things

---
+++ nvim/lua/config.lua
@@ -0,0 +1,24 @@
+local success, plugin = pcall(require, 'plugin')
+
+if success then
+  require('autopair_rc')
+  require('autotag_rc')
+  require('base')
+  require('buffer_line_rc')
+  require('colorizer_rc')
+  require('go_rc')
+  require('hydra_rc')
+  require('keymap')
+  require('lsp')
+  require('lspsaga_rc')
+  require('rc_cmp')
+  require('status_line_rc')
+  require('telescope')
+  require('ter_init')
+  require('virtual_type_rc')
+  require('cursor_line_rc')
+  require('ultisnip_rc')
+else
+  print('Erreur lors du chargement du plugin:', plugin)
+end
+
