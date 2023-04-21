3f5b7562a93abcf9eadbc5185d02a38b311a54a9
KyliianHardis <mayanga.kylian@gmail.com>
2023-04-21 22:45:01 +0200

    undind tmux navigate

--- fish/config.fish
+++ fish/config.fish
@@ -51,8 +51,6 @@ set -Ua fish_user_paths $HOME/.cargo/bin
 
 set -ga PATH $PATH /home/kylian/.local/bin
 
-bind \cp tmux_navigate
-
 # set -gx NVM_DIR "$HOME/.nvm"
 # source (bass -c 'source "$NVM_DIR/nvm.sh"') 2>/dev/null
 
