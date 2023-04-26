c67611fcd64899ee7a8eb38ec1e50b08c566357a
KyliianHardis <mayanga.kylian@gmail.com>
2023-04-26 13:18:18 +0200

    fix du problem des lenteur, le path etait trop long

--- fish/path.fish
+++ fish/path.fish
@@ -1,12 +1,34 @@
-set -x PATH $PATH /usr/local/bin
-set -gx PATH $PATH $GOPATH/bin
-set -gx GOPATH ~/go
-set -gx PATH /Users/kylianmayanga/go/bin $PATH
-set -U FZF_COMPLETE 2
-set -x GOPATH ~/go
-set -x GOBIN $GOPATH/bin/
-set -x PATH $PATH:$GOPATH:$GOBIN
-set -U fish_user_paths ~/bin $fish_user_paths
-set -g -x TMUX_CONF ~/.config/tmux/tmux.conf
-set -ga PATH $PATH /home/kylian/.local/bin
-set -Ua fish_user_paths $HOME/.cargo/bin
+# Add /usr/local/bin to PATH if it's not already there
+if not contains /usr/local/bin $PATH
+    set -x PATH $PATH /usr/local/bin
+end
+
+# Add $GOPATH/bin to PATH if it's not already there
+if not contains $GOPATH/bin $PATH
+    set -gx PATH $PATH $GOPATH/bin
+end
+
+# Add ~/go/bin to PATH if it's not already there
+if not contains ~/go/bin $PATH
+    set -gx PATH ~/go/bin $PATH
+end
+
+# Add ~/.local/bin to PATH if it's not already there
+if not contains ~/.local/bin $PATH
+    set -ga PATH $PATH ~/.local/bin
+end
+
+# Add $HOME/.cargo/bin to fish_user_paths if it's not already there
+if not contains $HOME/.cargo/bin $fish_user_paths
+    set -Ua fish_user_paths $HOME/.cargo/bin
+end
+
+# Set FZF_COMPLETE to 2 if it's not already set
+if not set -q FZF_COMPLETE
+    set -U FZF_COMPLETE 2
+end
+
+# Set TMUX_CONF to ~/.config/tmux/tmux.conf if it's not already set
+if not set -q TMUX_CONF
+    set -g -x TMUX_CONF ~/.config/tmux/tmux.conf
+end
