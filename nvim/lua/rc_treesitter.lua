875300c9f27ed0786be54867b5e0ada05e02a192
KyliianHardis <mayanga.kylian@gmail.com>
2023-04-28 13:48:45 +0200

    format

--- nvim/lua/rc_treesitter.lua
+++ nvim/lua/rc_treesitter.lua
@@ -1,23 +1,38 @@
+-- Load nvim-treesitter and get the configs module
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+	return
 end
 
+-- Ensure that the required parsers are installed
+require 'nvim-treesitter.install'.ensure_installed({
+	"go",
+	"lua",
+	"python",
+	"typescript",
+	"javascript",
+	"html",
+	"css",
+	"yaml",
+	"json",
+	"markdown",
+	"markdown_inline"
+})
+
+-- Configure nvim-treesitter with the desired settings
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true,
+		disable = { "" },
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 }
+
