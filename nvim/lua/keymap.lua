8b76ded3bedeb25d51b18d6402438ca2c93d04d7
Kylian Mayanga-Fournier <mayanga.kylian@gmail.com>
2023-05-18 17:19:16 +0200

    change the keymap of harpoon

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -152,12 +152,12 @@ if success then
 	local success2, ui = pcall(require, "harpoon.ui")
 
 	if success1 and success2 then
-		vim.keymap.set("n", "<A-;>", mark.add_file)
-		vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu)
-		vim.keymap.set("n", "<A-j>", function() ui.nav_file(1) end)
-		vim.keymap.set("n", "<A-u>", function() ui.nav_file(2) end)
-		vim.keymap.set("n", "<A-k>", function() ui.nav_file(3) end)
-		vim.keymap.set("n", "<A-i>", function() ui.nav_file(4) end)
+		vim.keymap.set("n", "<leader>a", mark.add_file)
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<C-k>", function() ui.nav_file(1) end)
+		vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+		vim.keymap.set("n", "<C-h>", function() ui.nav_file(3) end)
+		vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
 	else
 		print("Error loading Harpoon mark or UI module")
 	end
