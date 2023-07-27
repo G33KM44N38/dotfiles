d0776fbef5701b2e5c5a9201934cdc702cbd15b1
G33KM44N38 <mayanga.kylian@gmail.com>
2023-07-27 13:45:41 +0200

    modify the keymap

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -3,6 +3,7 @@ vim.o.termguicolors = true
 vim.cmd("set termguicolors")
 
 local opts = { noremap = true, silent = true }
+local noSilent = { noremap = true }
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -11,8 +12,8 @@ vim.g.maplocalleader = " "
 local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux_navigate<CR>", opts)
-keymap('n', '<leader>lg', '<cmd>silent !tmux neww lg<CR>', opts)
-keymap('n', '<leader>lzd', '<cmd>silent !tmux neww lzd<CR>', opts)
+keymap('n', '<leader>lg', '<cmd>silent !tmux neww lazygit<CR>', opts)
+keymap('n', '<leader>ld', '<cmd>silent !tmux neww lazydocker<CR>', opts)
 keymap('i', 'kj', '<Esc>', opts)
 keymap('i', 'KJ', '<Esc>', opts)
 
@@ -29,31 +30,29 @@ keymap("n", "<TAB>", ":bn<CR>", opts)
 -- make
 keymap("n", "<leader>m", ":make ", opts)
 
--- commante line
+-- commente line
 keymap("n", "<leader><leader>c", ":Commentary <CR>", opts)
 
 -- paste without overwrite
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- paste to the clipboard
-keymap("n", "<leader>y", "\"+y",opts)
-keymap("v", "<leader>y", "\"+y",opts)
-keymap("n", "<leader>Y", "\"+y",opts)
+keymap("n", "<leader>y", "\"+y", opts)
+keymap("v", "<leader>y", "\"+y", opts)
+keymap("n", "<leader>Y", "\"+y", opts)
 
 -- Increment/decrement
 keymap('n', '+', '<C-a>', opts)
 keymap('n', '-', '<C-x>', opts)
 
 -- mapping fugitif
-keymap("n", "<leader>gs", ":G<CR>", opts)
+keymap("n", "<leader>g", ":G<CR>", opts)
 keymap("n", "<leader>gc", ":Git commit<CR>", opts)
+keymap("n", "<leader>gs", ":Git status<CR>", opts)
 keymap("n", "<leader>gp", ":Git push<CR>", opts)
-keymap("n", "<leader>gb", ":Git checkout", opts)
-keymap("n", "<leader>gnb", ":Git checkout -b", opts)
-
--- split windows
-keymap("n", "<leader>sv", ":split<Return><C-w>w", opts)
-keymap("n", "<leader>sh", ":vsplit<Return><C-w>w", opts)
+keymap("n", "<leader>gP", ":Git pull<CR>", opts)
+keymap("n", "<leader>gC", ":Git checkout ", noSilent)
+keymap("n", "<leader>gCn", ":Git checkout -b ", noSilent)
 
 -- open term
 keymap("n", "<leader>tt", ":ToggleTerm direction=float fish<CR>", opts)
@@ -68,7 +67,7 @@ keymap("n", "<leader>bb", ":lua require'telescope.builtin'.buffers()<CR>", opts)
 keymap("n", "<leader>s", ":lua require('telescope.builtin').find_files()<CR>", opts)
 keymap("n", "<leader>dot", ":lua require('rc_telescope').search_dotfiles()<CR>", opts)
 keymap("n", "?", ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>", opts)
-keymap("n", "<leader><leader>s", ":lua require('telescope.builtin').live_grep()<CR>", opts)
+keymap("n", "<C-s>", ":lua require('telescope.builtin').live_grep()<CR>", opts)
 keymap("n", "<leader>bq", ":lua require('telescope.builtin').quickfix()<CR>", opts)
 keymap("n", "<leader>gf", ":lua require('telescope.builtin').git_files()<CR>", opts)
 keymap("n", "<leader>xx", ":lua require('telescope.builtin').diagnostics()<CR>", opts)
@@ -87,19 +86,20 @@ keymap("n", "<F5>", ":lua require'dap'.continue()<CR>", opts)
 keymap("n", "<F2>", ":lua require'dap'.step_over()<CR>", opts)
 keymap("n", "<F3>", ":lua require'dap'.step_into()<CR>", opts)
 keymap("n", "<F4>", ":lua require'dap'.step_out()<CR>", opts)
-keymap("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
-keymap("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
+keymap("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
+keymap("n", "<leader>dB", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
 keymap("n", "<leader>ds", ":lua require'dap-go'.debug_test()<CR>", opts)
-keymap("n", "<leader>du", ":lua require'dapui'.toggle()<CR>", opts)
+keymap("n", "<leader>dt", ":lua require'dapui'.toggle()<CR>", opts)
 
 -- lspsaga
-keymap("n", "gD", "<cmd>Lspsaga lsp_finder<CR>", opts)
-keymap("i", "<C-k>", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+keymap("n", "gD", "<cmd>Lspsaga finder<CR>", opts)
+keymap("n", "<C-l>", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 keymap("n", "<C-j>", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
+keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>", opts)
 
 -- vim-test
 keymap("n", "<leader>t", ":TestNearest -v<CR>", opts)
@@ -113,15 +113,14 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
 -- half page jumping, and keep the cursor in front
 keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz",opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
 
 -- allow search term to be in the middle
-keymap("n", "n", "nzzzv",opts)
-keymap("n", "N", "Nzzzv",opts)
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
 
 local function setup_mappings()
 	-- NERDTree mappings
-	keymap('n', '<leader>pv', ':NERDTree<CR>', opts)
 	keymap('n', '<leader>n', ':NERDTreeFocus<CR>', opts)
 	keymap('n', '<C-p>', ':NERDTreeToggle<CR>', opts)
 	keymap('n', '<leader>f', ':NERDTreeFind<CR>', opts)
@@ -152,4 +151,3 @@ if success then
 else
 	print('Erreur lors du chargement du plugin:', plugin)
 end
-
