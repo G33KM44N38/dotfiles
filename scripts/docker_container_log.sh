5fb2df01183146dabadb8e33e652342353f6f401
G33KM44N38 <mayanga.kylian@gmail.com>
2023-10-03 00:09:10 +0200

    change the starting point of the list so I don't miss container

--- scripts/docker_container_log.sh
+++ scripts/docker_container_log.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-container=$(docker ps --format "{{.Names}}" | awk 'NR>1')
+container=$(docker ps --format "{{.Names}}" | awk 'NR>0')
 selected=$(echo "$container" | fzf)
 
 if echo "$container" | grep -qs "$selected"; then
