237beba52283e3b70309a354175134991478140a
G33KM44N38 <mayanga.kylian@gmail.com>
2023-10-02 15:01:24 +0200

    change the notify for update in the plugin

--- nvim/lua/root/lazy.lua
+++ nvim/lua/root/lazy.lua
@@ -1,26 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
-	{import = "root.plugins"},
-	{import = "root.plugins.lsp"},
-},{
+	{ import = "root.plugins" },
+	{ import = "root.plugins.lsp" },
+}, {
 	install = {
 	},
 	checker = {
 		enabled = true,
-		notifiy = false,
+		notify = false, -- get a notification when new updates are found
+		-- frequency = 3600, -- check for updates every hour
+
 	},
 	change_detection = {
 		notify = false,
