d06ea076aa46d27398c3c5bc971227d0a00cd4ab
G33KM44N38 <mayanga.kylian@gmail.com>
2023-10-05 14:41:34 +0200

    modified sript logging docker

--- scripts/docker_container_log.sh
+++ scripts/docker_container_log.sh
@@ -4,7 +4,7 @@ container=$(docker ps --format "{{.Names}}" | awk 'NR>0')
 selected=$(echo "$container" | fzf)
 
 if echo "$container" | grep -qs "$selected"; then
-    tmux split-window -v -d "bash -c 'docker logs -f \"$selected\"; read'"
+    tmux split-window -v -d "bash -c 'docker logs -f \"$selected\"'"
 else
     echo "Container not found."
 fi
