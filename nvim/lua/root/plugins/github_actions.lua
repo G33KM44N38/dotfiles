a52510a184e4a2086b0daada9c73733792ee2ee8
G33KM44N38 <mayanga.kylian@gmail.com>
2023-10-05 14:42:06 +0200

    add a plugin to see my github workflow directly from my code

---
+++ nvim/lua/root/plugins/github_actions.lua
@@ -0,0 +1,14 @@
+return {
+	'topaxi/gh-actions.nvim',
+	cmd = 'GhActions',
+	keys = {
+		{ '<leader>gh', '<cmd>GhActions<cr>', desc = 'Open Github Actions' },
+	},
+	-- optional, you can also install and use `yq` instead.
+	build = 'make',
+	dependencies = { 'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim' },
+	opts = {},
+	config = function(_, opts)
+		require('gh-actions').setup(opts)
+	end
+}
