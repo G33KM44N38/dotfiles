923e010e28bdd33a307cc8ff2d7336ece58f1af3
G33KM44N38 <mayanga.kylian@gmail.com>
2023-11-03 21:27:07 +0100

    🚀 FEATURE:  🚀

--- nvim/lua/root/plugins/vim-fugitif.lua
+++ nvim/lua/root/plugins/vim-fugitif.lua
@@ -1,18 +1,18 @@
 return {
 	'tpope/vim-fugitive',
 	config = function()
-		function InputArgs()
+		function InputArgs(format)
 			local message = vim.fn.input('Enter your message: ')
 			if message ~= '' then
 				vim.cmd('echo "Message entered: "')
 				vim.cmd('echo "' .. message .. '"')
-				vim.cmd('G commit -v -m "🔧 FEATURE: ' .. message .. ' 🔧"')
+				vim.cmd('G commit -v -m "' .. format .. message .. '"')
 			else
 				vim.cmd('echo "No message entered."')
 			end
 		end
 
-		vim.cmd('command -bar -bang -nargs=* Gfix lua InputArgs()')
+		vim.cmd('command -bar -bang -nargs=* Gfix lua InputArgs("🔧 FEATURE: ")')
 		vim.cmd('command -bar -bang -nargs=* Gfeature :G commit<bang> -v -m "🚀 FEATURE: <args> 🚀"')
 	end
 }
