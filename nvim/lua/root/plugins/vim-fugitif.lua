866728727d59fad85df464691caa639ed87ec6b5
G33KM44N38 <mayanga.kylian@gmail.com>
2023-11-03 21:07:01 +0100

    🔧 FIX: call InputArgs() 🔧

--- nvim/lua/root/plugins/vim-fugitif.lua
+++ nvim/lua/root/plugins/vim-fugitif.lua
@@ -1,7 +1,13 @@
+function InputArgs()
+	local message = vim.fn.input('🔧 FIX: ')
+	vim.fn.execute('G commit' .. (vim.fn.v:count() > 0 and '!' or '') .. ' -v -m "🔧 FIX: ' .. message .. ' 🔧"')
+end
+
 return {
+
 	'tpope/vim-fugitive',
 	config = function()
-		vim.cmd('command -bar -bang -nargs=* Gfix :G commit<bang> -v -m "🔧 FIX: <args> 🔧"')
+		vim.cmd('command -bar -bang -nargs=* Gfix :G commit<bang> -v -m "🔧 FIX: call InputArgs() 🔧"')
 		vim.cmd('command -bar -bang -nargs=* Gfeature :G commit<bang> -v -m "🚀 FEATURE: <args> 🚀"')
 	end
 }
