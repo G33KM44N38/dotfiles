daca6c2dec153db48f7c4b60d0c2f5c716af0d59
G33KM44N38 <mayanga.kylian@gmail.com>
2024-01-04 05:05:39 +0100

    🖌 APPEARANCE: style

--- .config/install/install.sh
+++ .config/install/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#color codes
+# Color codes
 RED='\033[1;31m'
 YELLOW='\033[1;33m'
-BLUE="\\033[38;5;27m"
-SEA="\\033[38;5;49m"
+BLUE='\033[38;5;27m'
+SEA='\033[38;5;49m'
 GREEN='\033[1;32m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
-#emoji codes
+# Emoji codes
 CHECK_MARK="${GREEN}\xE2\x9C\x94${NC}"
 X_MARK="${RED}\xE2\x9C\x96${NC}"
 PIN="${RED}\xF0\x9F\x93\x8C${NC}"
@@ -20,8 +20,7 @@ HOT="${ORANGE}\xF0\x9F\x94\xA5${NC}"
 WARNING="${RED}\xF0\x9F\x9A\xA8${NC}"
 RIGHT_ANGLE="${GREEN}\xE2\x88\x9F${NC}"
 
-
-#path
+# Path
 DOTFILES_DIR="$HOME/.dotfiles"
 
 set -e
@@ -31,7 +30,7 @@ handle_error() {
   local exit_code=$1
   local error_message=$2
   if [ $exit_code -ne 0 ]; then
-    echo "Error: $error_message" >&2
+    echo -e "${RED}Error: $error_message${NC}" >&2
     exit 1
   fi
 }
@@ -48,7 +47,7 @@ fi
 # Check if Homebrew is installed
 if ! command -v brew &> /dev/null
 then
-    echo "${ARROW} ${CYAN} Homebrew is not installed. Installing Homebrew..."
+    echo -e "${ARROW} ${CYAN}Homebrew is not installed. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     handle_error $? "Homebrew installation failed"
 fi
@@ -57,14 +56,13 @@ fi
 echo 'which brew' >> /Users/kylianmayanga/.zprofile
 
 # Install Ansible
-echo "${ARROW} ${CYAN} Installing Ansible..."
+echo -e "${ARROW} ${CYAN}Installing Ansible..."
 brew install ansible
 handle_error $? "Ansible installation failed"
 
-
 # Run the Ansible playbook
-echo "${ARROW} ${CYAN} Running the Ansible playbook..."
-ansible-playbook ~/.dotfiles/.config/install/main.yaml --ask-become-pass
+echo -e "${ARROW} ${CYAN}Running the Ansible playbook..."
+ansible-playbook "$DOTFILES_DIR/.config/install/main.yaml" --ask-become-pass
 handle_error $? "Ansible playbook execution failed"
 
-echo "${ARROW} ${CYAN} Installation and playbook execution completed successfully"
+echo -e "${ARROW} ${CYAN}Installation and playbook execution completed successfully"
