c594faaefb671c377a7fb6e032f2a0506a0dee6e
G33KM44N38 <mayanga.kylian@gmail.com>
2024-01-04 03:20:48 +0100

    oe

--- .config/install/install.sh
+++ .config/install/install.sh
@@ -10,12 +10,17 @@ handle_error() {
   fi
 }
 
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+handle_error $? "Oh My Zsh installation failed"
 
 # Check if Homebrew is installed
-echo "Homebrew is not installed. Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-handle_error $? "Homebrew installation failed"
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew is not installed. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    handle_error $? "Homebrew installation failed"
+fi
 
 # Add Homebrew to the PATH
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kylianmayanga/.zprofile
@@ -31,8 +36,8 @@ echo "Installing Ansible..."
 brew install ansible
 handle_error $? "Ansible installation failed"
 
-#Clone dotfils
-echo "Clone dotfiles"
+# Clone dotfiles
+echo "Cloning dotfiles..."
 git clone https://github.com/G33KM44N38/dotfiles ~/.dotfiles
 
 # Run the Ansible playbook
@@ -41,4 +46,3 @@ ansible-playbook ~/.dotfiles/.config/install/dev-env.yaml --ask-become-pass
 handle_error $? "Ansible playbook execution failed"
 
 echo "Installation and playbook execution completed successfully"
-
