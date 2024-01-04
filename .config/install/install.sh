bf1c80fe62db198be4eb0e06390332adbf6ad85c
G33KM44N38 <mayanga.kylian@gmail.com>
2024-01-04 02:16:36 +0100

    change the become pass

--- .config/install/install.sh
+++ .config/install/install.sh
@@ -34,7 +34,7 @@ handle_error $? "Ansible installation failed"
 
 # Run the Ansible playbook
 echo "Running the Ansible playbook..."
-ansible-playbook dev-env.yaml --ask-become-yes
+ansible-playbook dev-env.yaml --ask-become-pass
 handle_error $? "Ansible playbook execution failed"
 
 # Check if Zsh is installed
