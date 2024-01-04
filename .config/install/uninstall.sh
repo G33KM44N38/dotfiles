02d37bfc75c8bc0d4b2c162804f62aca07cfb838
G33KM44N38 <mayanga.kylian@gmail.com>
2024-01-04 02:07:54 +0100

    make it more robust with perplexity

--- .config/install/uninstall.sh
+++ .config/install/uninstall.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-chsh -s /bin/bash
-
 # Function to log messages
 log() {
   echo "$(date) - $1" >> /var/log/custom_uninstall.log
 }
 
+# Function to check for errors and log messages
+check_error() {
+  if [ $1 -ne 0 ]; then
+    log "Error: $2 failed"
+    exit 1
+  fi
+}
+
+# Set the default shell to bash
+chsh -s /bin/bash
+check_error $? "Changing default shell to bash"
+
 # Uninstall Oh My Zsh
 log "Uninstalling Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
-if [ $? -ne 0 ]; then
-  log "Error: Oh My Zsh uninstallation failed"
-  exit 1
-fi
+check_error $? "Oh My Zsh uninstallation"
 
 # Uninstall Ansible
 log "Uninstalling Ansible..."
 brew uninstall ansible
-if [ $? -ne 0 ]; then
-  log "Error: Ansible uninstallation failed"
-  exit 1
-fi
+check_error $? "Ansible uninstallation"
 
 # Uninstall Python
 log "Uninstalling Python..."
 brew uninstall python
-if [ $? -ne 0 ]; then
-  log "Error: Python uninstallation failed"
-  exit 1
-fi
+check_error $? "Python uninstallation"
 
 # Uninstall Homebrew
 log "Uninstalling Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-if [ $? -ne 0 ]; then
-  log "Error: Homebrew uninstallation failed"
-  exit 1
-fi
+check_error $? "Homebrew uninstallation"
 
 log "Uninstallation completed successfully"
