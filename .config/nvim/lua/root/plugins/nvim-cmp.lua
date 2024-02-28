8f4117dfc9188dbdbf7deb845cab876774c59fd9
G33KM44N38 <mayanga.kylian@gmail.com>
2024-02-28 13:56:01 +0100

    🔧 FIX: optmize performance

--- .config/nvim/lua/root/plugins/nvim-cmp.lua
+++ .config/nvim/lua/root/plugins/nvim-cmp.lua
@@ -54,11 +54,11 @@ return {
 			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "copilot" },
-				{ name = "ultisnips" },
+				{ name = "ultisnips", group_index = 1 },
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
+				{ name = "luasnip",   group_index = 1 }, -- snippets
+				{ name = "buffer" },     -- text within current buffer
+				{ name = "path" },       -- file system paths
 			}),
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
