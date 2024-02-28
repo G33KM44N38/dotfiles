7cb925b1d40405b41545b90cf504c6b9ed0da7da
G33KM44N38 <mayanga.kylian@gmail.com>
2024-02-28 21:33:25 +0100

    🚀 FEATURE: add command to raycast

---
+++ bin/lunch-dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title  dev
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description  dev
+# @raycast.author me
+
+open -a "Docker"
+open -a "Arc"
+open -a "iTerm"
