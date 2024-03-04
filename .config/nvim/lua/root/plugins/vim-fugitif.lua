0bc05d6f2f03a0018d223ce8b69e109b54849a41
G33KM44N38 <mayanga.kylian@gmail.com>
2024-03-04 18:06:17 +0100

    feat: allow a list of value to be committed✨

--- .config/nvim/lua/root/plugins/vim-fugitif.lua
+++ .config/nvim/lua/root/plugins/vim-fugitif.lua
@@ -1,23 +1,45 @@
 return {
 	'tpope/vim-fugitive',
 	config = function()
-		function InputArgs(format)
-			local message = vim.fn.input(format)
-			if message ~= '' then
-				vim.cmd('echo "Message entered: "')
-				vim.cmd('echo "' .. message .. '"')
-				vim.cmd('G commit -v -m "' .. format .. message .. '"')
+		local menu_options = {
+			{ key = 'build', desc = 'Build', emoji = '🏗️' },
+			{ key = 'ci', desc = 'Continuous Integration', emoji = '🔄' },
+			{ key = 'docs', desc = 'Documentation', emoji = '📚' },
+			{ key = 'feat', desc = 'Feature', emoji = '✨' },
+			{ key = 'fix', desc = 'Bug Fix', emoji = '🐛' },
+			{ key = 'perf', desc = 'Performance Improvement', emoji = '🚀' },
+			{ key = 'refactor', desc = 'Code Refactoring', emoji = '🧹' },
+			{ key = 'test', desc = 'Testing', emoji = '🧪' }
+		}
+
+		function ShowMenu()
+			local menu_items = { 'Select a commit type:' }
+			for index, option in ipairs(menu_options) do
+				table.insert(menu_items, index .. '. ' .. option.desc)
+			end
+			local choice = vim.fn.inputlist(menu_items)
+			return choice > 0 and menu_options[choice] or nil -- Return the whole option object.
+		end
+
+		function InputArgs()
+			local option = ShowMenu() -- This will now hold the entire selected option object.
+			if option then
+				local message = vim.fn.input('Message: ')
+				if message ~= '' then
+					vim.cmd('echo "Message entered: "')
+					vim.cmd('echo "' .. message .. '"')
+					local commitMessage = option.emoji ..
+					    ' ' .. option.key .. ': ' .. message -- Include the emoji.
+					vim.cmd('G commit -v -m "' .. commitMessage .. '"')
+				else
+					vim.cmd('echo "No message entered. Action canceled"')
+				end
 			else
-				vim.cmd('echo "No message entered. Action canceled"')
+				vim.cmd('echo "No commit type selected. Action canceled"')
 			end
 		end
 
-
-		vim.cmd('command -bar -bang -nargs=* Gfix lua InputArgs("🔧 FIX: ")')
-		vim.cmd('command -bar -bang -nargs=* Gfeature lua InputArgs("🚀 FEATURE: ")')
-		vim.cmd('command -bar -bang -nargs=* Gstyle lua InputArgs("🖌 APPEARANCE: ")')
-		vim.cmd('command -bar -bang -nargs=* Gtest lua InputArgs("🧪 UNIT-TEST: ")')
-		vim.cmd('command -bar -bang -nargs=* Grefacto lua InputArgs("🧨 REFACTO: ")')
-		vim.cmd('command -bar -bang -nargs=* Gdebug lua InputArgs("💬 DEBUG: " )')
+		vim.cmd('command -bar -bang -nargs=* Gcommit lua InputArgs()')
+		vim.api.nvim_set_keymap('n', '<leader>gc', ':Gcommit<CR>', { noremap = true, silent = true })
 	end
 }
