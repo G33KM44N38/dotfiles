173e1aed80a1213e9970e9d5ce27a5b673f38255
G33KM44N38 <mayanga.kylian@gmail.com>
2024-03-04 18:16:37 +0100

    refactor: delete comment 🛠️

--- .config/nvim/lua/root/plugins/vim-fugitif.lua
+++ .config/nvim/lua/root/plugins/vim-fugitif.lua
@@ -3,38 +3,42 @@ return {
 	config = function()
 		local menu_options = {
 			{ key = 'build', desc = 'Build', emoji = '🏗️' },
-			{ key = 'ci', desc = 'Continuous Integration', emoji = '🔄' },
+			{ key = 'ci', desc = 'Continuous Integration', emoji = '🤖' },
 			{ key = 'docs', desc = 'Documentation', emoji = '📚' },
 			{ key = 'feat', desc = 'Feature', emoji = '✨' },
 			{ key = 'fix', desc = 'Bug Fix', emoji = '🐛' },
-			{ key = 'perf', desc = 'Performance Improvement', emoji = '🚀' },
-			{ key = 'refactor', desc = 'Code Refactoring', emoji = '🧹' },
+			{ key = 'perf', desc = 'Performance Improvement', emoji = '⚡' },
+			{ key = 'refactor', desc = 'Code Refactoring', emoji = '🛠️' },
 			{ key = 'test', desc = 'Testing', emoji = '🧪' }
 		}
 
 		function ShowMenu()
 			local menu_items = { 'Select a commit type:' }
 			for index, option in ipairs(menu_options) do
-				table.insert(menu_items, index .. '. ' .. option.desc)
+				table.insert(menu_items, index .. '. ' .. option.emoji .. ' ' .. option.desc)
 			end
 			local choice = vim.fn.inputlist(menu_items)
-			return choice > 0 and menu_options[choice] or nil -- Return the whole option object.
+			return choice > 0 and menu_options[choice] or nil
 		end
 
 		function InputArgs()
-			local option = ShowMenu() -- This will now hold the entire selected option object.
-			if option then
+			local selected_option = ShowMenu()
+			if selected_option then
+				vim.cmd('redraw')
+				vim.cmd('echo "You selected: ' .. selected_option.emoji .. ' - ' .. selected_option.key .. '"')
 				local message = vim.fn.input('Message: ')
 				if message ~= '' then
+					vim.cmd('redraw') -- Optionally redraw again if needed
 					vim.cmd('echo "Message entered: "')
 					vim.cmd('echo "' .. message .. '"')
-					local commitMessage = option.emoji ..
-					    ' ' .. option.key .. ': ' .. message -- Include the emoji.
+					local commitMessage = selected_option.key .. ': ' .. message .. ' ' .. selected_option.emoji
 					vim.cmd('G commit -v -m "' .. commitMessage .. '"')
 				else
+					vim.cmd('redraw') -- Clear the screen before showing the message
 					vim.cmd('echo "No message entered. Action canceled"')
 				end
 			else
+				vim.cmd('redraw') -- Clear the screen before showing the message
 				vim.cmd('echo "No commit type selected. Action canceled"')
 			end
 		end
