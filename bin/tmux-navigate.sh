0168d1218248ac57a19c85bdd9cc7e758eb6d336
G33KM44N38 <mayanga.kylian@gmail.com>
2024-03-14 22:53:59 +0100

    fix: fix tmux-navigate 🐛

--- bin/tmux-navigate.sh
+++ bin/tmux-navigate.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
 
-# Function to display error and exit
-error_exit() {
-  echo "Error: $1" >&2
-  exit 1
-}
-
-# Check if fzf is installed
-if ! command -v fzf &> /dev/null; then
-    error_exit "fzf is not installed. Please install fzf to use this script."
-fi
-
-# Check the number of arguments
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find \
+selected=$(find \
+    ~/keyboard/ \
     ~/coding/ \
     ~/coding/* \
     ~/coding/work/minata/ \
@@ -25,23 +14,23 @@ else
     ~/.dotfiles/* \
     ~/.dotfiles/.config/ \
     ~/SecondBrain/ \
-    -mindepth 1 -maxdepth 1 -type d | fzf) || error_exit "No directory selected."
+    -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
-# Check if a selection is made
 if [[ -z $selected ]]; then
-    error_exit "No directory selected."
+    exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-#Check if tmux is running
-if [[ -n $TMUX ]] || [[ -n $tmux_running ]]; then
-    if ! tmux has-session -t=$selected_name 2> /dev/null; then
-        tmux new-session -ds $selected_name -c $selected
-    fi
-    tmux switch-client -t $selected_name
-else
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected
 fi
+
+tmux switch-client -t $selected_name
