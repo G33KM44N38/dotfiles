bcaf61ad7b4311927d0bd581ba2a0d138c1f8d8c
G33KM44N38 <mayanga.kylian@gmail.com>
2024-05-15 15:27:49 +0200

    refactor(keymap): refactor only for esthetism

--- .config/nvim/lua/root/core/keymap.lua
+++ .config/nvim/lua/root/core/keymap.lua
@@ -70,8 +70,7 @@ keymap("n", "J", "mzJ`z", opts)
 keymap("n", ";f", "<cmd>cprev<CR>", opts)
 keymap("n", ";j", "<cmd>cnext<CR>", opts)
 
-opts.desc = "See available code actions"
-vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+vim.keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, { desc = "See available code actions" }) -- see available code actions, in visual mode will apply to selection
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
