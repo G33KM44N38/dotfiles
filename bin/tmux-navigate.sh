a57388565c383477ff5ca9d89bb718214a496797
kylian <kylian@MBP-de-Kael-001.lan>
2024-06-23 01:35:51 +0200

    fixup! fix(tmux-navigate): delete secondBrain and keyboard

--- bin/tmux-navigate.sh
+++ bin/tmux-navigate.sh
@@ -4,14 +4,12 @@ if [[ $# -eq 1 ]]; then
     selected=$1
 else
 selected=$(find \
-    # ~/keyboard/ \
     ~/coding/ \
     ~/coding/* \
     ~/goinfre/ \
     ~/.dotfiles/ \
     ~/.dotfiles/* \
     ~/.dotfiles/.config/ \
-    # ~/SecondBrain/ \
     -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
