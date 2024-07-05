8fe7963baf934a65cf21b2e8c880daddd475881b
G33KM44N38 <mayanga.kylian@gmail.com>
2024-07-05 10:48:38 +0200

    feat(raycast): open music app

---
+++ bin/open_music.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title  Music
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📀
+
+# Documentation:
+# @raycast.description  open Music app
+# @raycast.author me
+
+open -a "Music"
