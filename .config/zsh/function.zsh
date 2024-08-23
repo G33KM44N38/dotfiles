f90a286582ed41e156c0f32297a96aabd425ab60
G33KM44N38 <kylianmayanga@gmail.com>
2024-08-23 16:49:01 +0200

    feat(npm): get the completion for package.json file

--- .config/zsh/function.zsh
+++ .config/zsh/function.zsh
@@ -18,3 +18,41 @@ decrypt_env() {
     local filename=$1
     sops --decrypt --age $(grep -oE "public key: (.*)" "$SOPS_AGE_KEY_FILE" | sed 's/public key: //') -i "$filename"
 }
+
+# Add this to your ~/.zshrc file
+
+# Function to parse package.json and provide completions
+_package_json_completion() {
+  local curcontext="$curcontext" state line
+  typeset -A opt_args
+
+  _arguments \
+    '1: :->command' \
+    '*: :->argument'
+
+  case $state in
+    command)
+      local commands
+      commands=($(jq -r 'keys | .[]' package.json 2>/dev/null))
+      _describe -t commands 'package.json commands' commands
+      ;;
+    argument)
+      case $words[2] in
+        scripts)
+          local scripts
+          scripts=($(jq -r '.scripts | keys | .[]' package.json 2>/dev/null))
+          _describe -t scripts 'npm scripts' scripts
+          ;;
+        dependencies|devDependencies)
+          local deps
+          deps=($(jq -r ".$words[2] | keys | .[]" package.json 2>/dev/null))
+          _describe -t dependencies "$words[2]" deps
+          ;;
+      esac
+      ;;
+  esac
+}
+
+# Define the completion for package.json
+compdef _package_json_completion jq -c '.' package.json
+
