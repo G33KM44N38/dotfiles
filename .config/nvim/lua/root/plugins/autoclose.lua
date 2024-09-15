5408b4c0e58d1b7cb9d9be5d4cd43ff5758af661
G33KM44N38 <kylianmayanga@gmail.com>
2024-09-15 21:42:12 +0200

    feat(autoclose): add autoclose

--- .config/nvim/lua/root/plugins/autoclose.lua
+++ .config/nvim/lua/root/plugins/autoclose.lua
@@ -1,7 +1,7 @@
 return {
-	-- "m4xshen/autoclose.nvim",
-	-- event = "InsertEnter",
-	-- config = function()
-	-- 	require("autoclose").setup()
-	-- end
+	"m4xshen/autoclose.nvim",
+	event = "InsertEnter",
+	config = function()
+		require("autoclose").setup()
+	end
 }
