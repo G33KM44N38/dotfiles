686ed31ebe69031795a59f8baa3973a72862ea40
G33KM44N38 <kylianmayanga@gmail.com>
2024-09-18 11:15:30 +0200

    refactor(lunch-dev-1): update the dev script to lunch my youtube playlist  concenration

---
+++ bin/lunch-dev-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title  dev
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description  dev
+# @raycast.author me
+
+open -a "Docker"
+open https://www.youtube.com/playlist?list=PLpatcLkBHFDXmfbHEkTYH4RIEUq3PU8us
+open -a "iTerm"
