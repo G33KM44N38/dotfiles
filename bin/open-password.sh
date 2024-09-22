84e7a9b68e2e865417daee4e204b32c6cacc13a3
G33KM44N38 <kylianmayanga@gmail.com>
2024-09-22 20:17:54 +0200

    feat(passwords): add open-password.sh script

---
+++ bin/open-password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title  Passwords
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.iconk🔑
+
+# Documentation:
+# @raycast.description  passwords
+# @raycast.author me
+
+open /System/Applications/Passwords.app
