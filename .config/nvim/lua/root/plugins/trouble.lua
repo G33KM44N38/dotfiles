82c2e536aca770c59c6c9b2a197803a9e4d52d5b
G33KM44N38 <kylianmayanga@gmail.com>
2024-10-11 09:17:40 +0200

    refactor(trouble): configure trouble keymaps

--- .config/nvim/lua/root/plugins/trouble.lua
+++ .config/nvim/lua/root/plugins/trouble.lua
@@ -1,9 +1,22 @@
 return {
 	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xw",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xb",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
 }
