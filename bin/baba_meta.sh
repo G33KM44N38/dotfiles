06aaf148023ce09c2e43515afc56f4e8ec635dd3
G33KM44N38 <kylianmayanga@gmail.com>
2024-12-16 02:27:27 +0100

    feat(raycast): add baba_meta.sh for opening baba profile meta

---
+++ bin/baba_meta.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title  baba meta
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 💈
+
+# Documentation:
+# @raycast.description  open meta
+# @raycast.author me
+#
+
+arc-cli select-space-name babacoiffure
+open https://business.facebook.com/latest/home?asset_id=528718700316175&business_id=159322119930574
+
+# vi:ft=sh:
