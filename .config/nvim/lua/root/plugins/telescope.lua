5f0e52e19f5925989641b314e6c0a6905f36c607
Kylian <kylian@MacBookPro.lan>
2025-01-23 18:26:16 +0100

    fix(telescope): Update file ignore patterns to include .next directory
    Modify the file ignore patterns in Telescope configuration to exclude the `.next` directory.
    
    Changes include:
    	• Added `.next/` to the `file_ignore_patterns` in `telescope.lua`
    
    Motivation:
    	• Prevent `.next` build artifacts from appearing in Telescope file searches, improving search relevance and efficiency for Next.js projects

--- .config/nvim/lua/root/plugins/telescope.lua
+++ .config/nvim/lua/root/plugins/telescope.lua
@@ -57,7 +57,7 @@ return {
 					preview_cutoff = 120,
 				},
 				file_sorter = require("telescope.sorters").get_fuzzy_file,
-				file_ignore_patterns = { "node_modules/", ".git/", "dist/", "go.sum", "package-lock.json", "lazy-lock.json", "target", "Cargo.lock" },
+				file_ignore_patterns = { "node_modules/", ".git/", "dist/", "go.sum", "package-lock.json", "lazy-lock.json", "target", "Cargo.lock", ".next/" },
 				generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
 				path_display = { "absolute" },
 				winblend = 0,
