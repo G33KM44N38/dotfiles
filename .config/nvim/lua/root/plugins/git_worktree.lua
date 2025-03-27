c4894e84fdbe1ce5c75ee5f97274fad93c9ce64a
kylian <kylianmayanga@gmail.com>
2025-03-27 12:55:25 +0100

    feat(nvim): Enhance git worktree integration for VSCode and Neovim
    Added support for VSCode users by mapping git worktree commands to VSCode actions. For Neovim users, improved key mappings for git worktree functionalities and added a function to update tmux windows when switching worktrees.
    
    - Mapped `<leader>ws`, `<leader>wc`, and `<leader>wd` for VSCode integration
    - Created key mappings for Neovim users to manage git worktrees with telescope
    - Implemented a function to restart LSP and update tmux windows on worktree switch

--- .config/nvim/lua/root/plugins/git_worktree.lua
+++ .config/nvim/lua/root/plugins/git_worktree.lua
@@ -1,26 +1,48 @@
 return {
 	"ThePrimeagen/git-worktree.nvim",
 	config = function()
-		require("telescope").load_extension("git_worktree")
-		local Worktree = require("git-worktree")
+		if vim.g.vscode then
+			vim.keymap.set("n", "<leader>ws", function()
+				require('vscode').action('git-worktree.list')
+			end)
+			vim.keymap.set("n", "<leader>wc", function()
+				require('vscode').action('git-worktree.add')
+			end)
+			vim.keymap.set("n", "<leader>wd", function()
+				require('vscode').action('git-worktree.remove')
+			end)
+		else
+			require("telescope").load_extension("git_worktree")
+			local Worktree = require("git-worktree")
+			local opts = { noremap = true, silent = true }
 
-		-- op = Operations.Switch, Operations.Create, Operations.Delete
-		-- metadata = table of useful values (structure dependent on op)
-		--      Switch
-		--          path = path you switched to
-		--          prev_path = previous worktree path
-		--      Create
-		--          path = path where worktree created
-		--          branch = branch name
-		--          upstream = upstream remote name
-		--      Delete
-		--          path = path where worktree deleted
-		--
-
-		Worktree.on_tree_change(function(op, metadata)
-			if op == Worktree.Operations.Switch then
-				print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+			-- Function to update tmux windows in the current client
+			local function update_tmux_windows()
+				os.execute("tmux kill-window -t 2")
+				os.execute("tmux kill-window -t 3")
+				os.execute("tmux new-window")
+				os.execute("tmux new-window")
+				os.execute("tmux select-window -t 1")
 			end
-		end)
+
+			vim.api.nvim_set_keymap("n", "<leader>ws",
+				":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", opts)
+			vim.api.nvim_set_keymap("n", "<leader>wc",
+				":lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", opts)
+			vim.api.nvim_set_keymap("n", "<leader>wt",
+				":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", opts)
+
+			Worktree.on_tree_change(function(op, metadata)
+				if op == Worktree.Operations.Switch then
+					print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+
+					-- Restart LSP
+					vim.api.nvim_command("LspRestart")
+
+					-- Update tmux windows in the current client to the new worktree path
+					update_tmux_windows()
+				end
+			end)
+		end
 	end
 }
