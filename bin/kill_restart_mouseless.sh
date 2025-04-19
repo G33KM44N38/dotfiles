e67e7f1c1ee2f9d7741eaa48935c5f5297adf755
kylian <kylianmayanga@gmail.com>
2025-04-19 20:57:31 +0200

    feat(scripts): add kill_restart_mouseless.sh script
    Introduced a new script to kill and restart the "mouseless" application. This functionality enhances workflow efficiency for users relying on this tool.
    
    - Created a bash script to terminate and reopen the application
    - Included Raycast metadata for easy integration
    - Ensured the script is executable and properly documented

---
+++ bin/kill_restart_mouseless.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title  kill restart mouseless
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🐁
+
+# Documentation:
+# @raycast.description  kill restart mouseless
+# @raycast.author me
+#
+
+pkill -f "mouseless" && open -a "mouseless"
+# vi:ft=sh:
