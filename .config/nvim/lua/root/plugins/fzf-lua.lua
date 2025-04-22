220be93b308dd57c279ccf7b65cb30e58649826b
kylian <kylianmayanga@gmail.com>
2025-04-22 13:25:19 +0200

    feat(nvim): Add LSP key mappings for implementations, definitions, and references
    Added key mappings for LSP functionalities to enhance navigation and improve user experience. New mappings include:
    
    - `gi` for LSP implementations
    - `gd` for LSP definitions
    - `gr` for LSP references
    
    Updated existing key mappings for file search and live grep to maintain consistency. Ensured the use of the `Opts` variable for key mapping options throughout.

--- .config/nvim/lua/root/plugins/fzf-lua.lua
+++ .config/nvim/lua/root/plugins/fzf-lua.lua
@@ -3,8 +3,11 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
 	config = function()
-		vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>FzfLua files<CR>", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>FzfLua live_grep<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", Opts)
+		vim.api.nvim_set_keymap("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", Opts)
+		vim.api.nvim_set_keymap("n", "gr", "<cmd>FzfLua lsp_references<CR>", Opts)
+		vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>FzfLua files<CR>", Opts)
+		vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>FzfLua live_grep<CR>", Opts)
 		vim.api.nvim_set_keymap("n", "<leader>ke", "<cmd>FzfLua keymaps<CR>",
 			{ noremap = true, silent = true })
 	end
--- .config/nvim/lua/root/plugins/lspsaga.lua
+++ .config/nvim/lua/root/plugins/lspsaga.lua
@@ -3,11 +3,7 @@ return {
 	branch = "main",
 	config = function()
 		require('lspsaga').setup({})
-		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-		vim.keymap.set("n", 'gr', "<cmd>Lspsaga finder<CR>", opts)
-		vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
-
+		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", Opts) -- show documentation for what is under cursor
 
 		vim.keymap.set('n', ']d', "<cmd>Lspsaga diagnostic_jump_next<CR>",
 			{ desc = 'Go to previous [D]iagnostic message' })
