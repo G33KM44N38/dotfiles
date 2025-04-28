e9ed334dcd926cbf54e5eee758ee0291f6ccfd6f
kylian <kylianmayanga@gmail.com>
2025-04-28 13:18:42 +0200

    refactor(formatter): Enhance formatter configuration and dependencies
    Improved the configuration of the formatter plugin by organizing dependencies and cleaning up commented code.
    
    - Added a new dependency for mason-conform with a setup function.
    - Streamlined the javascript_formatter definition and removed unnecessary comments.
    - Consolidated user commands for enabling and disabling autoformatting with clearer syntax.

--- .config/nvim/lua/root/plugins/formatter.lua
+++ .config/nvim/lua/root/plugins/formatter.lua
@@ -1,12 +1,22 @@
-local javascript_formatter= {
+local javascript_formatter = {
 	"prettier",
-	"ts_ls"
+	-- "ts_ls",
 }
+
 return {
 	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile", "InsertLeave" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		{
+			"zapling/mason-conform.nvim",
+			dependencies = { "williamboman/mason.nvim", "stevearc/conform.nvim" },
+			config = function()
+				require("mason-conform").setup({})
+			end,
+		},
+	},
 	config = function()
 		local conform = require("conform")
 		conform.setup({
@@ -16,14 +26,7 @@ return {
 				javascriptreact = javascript_formatter,
 				typescriptreact = javascript_formatter,
 				svelte = javascript_formatter,
-				-- css = { "prettier" },
-				-- html = { "prettier" },
-				-- json = { "prettier" },
-				-- yaml = { "prettier" },
-				-- markdown = { "prettier" },
-				-- graphql = { "prettier" },
 				lua = { "stylua" },
-				-- python = { "isort", "black" },
 			},
 			format_after_save = {
 				lsp_fallback = true,
@@ -31,6 +34,7 @@ return {
 				timeout_ms = 2000,
 			},
 		})
+
 		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
 			conform.format({
 				lsp_fallback = true,
@@ -45,25 +49,11 @@ return {
 			else
 				vim.g.disable_autoformat = true
 			end
-		end, {
-			desc = "Disable autoformat-on-save",
-			bang = true,
-		})
+		end, { desc = "Disable autoformat-on-save", bang = true })
+
 		vim.api.nvim_create_user_command("FormatEnable", function()
 			vim.b.disable_autoformat = false
 			vim.g.disable_autoformat = false
-		end, {
-			desc = "Re-enable autoformat-on-save",
-		})
+		end, { desc = "Re-enable autoformat-on-save" })
 	end,
-	{
-		"zapling/mason-conform.nvim",
-		dependencies = { "williamboman/mason.nvim", "stevearc/conform.nvim" },
-		config = function()
-			require("mason-conform").setup({
-				-- Optionally, specify formatters to ignore during installation
-				-- ignore_install = { "prettier" },
-			})
-		end,
-	},
 }
