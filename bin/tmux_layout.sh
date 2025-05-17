48d0a443f25cf744d5daeb6588af4dedcb981195
kylian <kylianmayanga@gmail.com>
2025-05-17 19:12:13 +0200

    refactor: cleanup the file for tmux_layout

--- bin/tmux_layout.sh
+++ bin/tmux_layout.sh
@@ -2,8 +2,6 @@
 
 tmux rename-window 'nvim'
 tmux send-keys -R "nvim ." C-m
-# tmux kill-window -t 3
-# tmux kill-window -t 2
 tmux new-window -n 'run'
 tmux new-window -n 'process'
 tmux select-window -t 1
