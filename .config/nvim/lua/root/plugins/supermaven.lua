c0a6c5419aba2266da62a5c0d87298358d64d472
kylian <kylianmayanga@gmail.com>
2025-05-25 11:24:39 +0200

    fix(supermaven): automatically restart Supermaven on non-markdown buffers
    Add autocmd to restart Supermaven when entering buffers with non-markdown
    filetypes. Prevents the need to manually run :SupermavenStart after editing
    markdown files, which globally stop Supermaven.

--- .config/nvim/lua/root/plugins/supermaven.lua
+++ .config/nvim/lua/root/plugins/supermaven.lua
@@ -9,10 +9,21 @@ return {
 			},
 		})
 
+		-- Stop Supermaven in markdown buffers
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "markdown",
 			callback = function()
 				vim.cmd("silent! SupermavenStop")
+			end,
+		})
+
+		-- Start Supermaven in other filetypes
+		vim.api.nvim_create_autocmd("BufEnter", {
+			callback = function()
+				local ft = vim.bo.filetype
+				if ft ~= "markdown" then
+					vim.cmd("silent! SupermavenStart")
+				end
 			end,
 		})
 	end,
