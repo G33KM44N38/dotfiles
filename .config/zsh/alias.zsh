4da9b32c6462d3835bec297348b0a236c417e3ab
kylian <kylianmayanga@gmail.com>
2025-11-26 21:33:18 +0100

    chore(zsh): Update git aliases in alias.zsh
    Modified Git command aliases to enhance usability and functionality.
    
    - Added `gcn` alias for `git commit --no-verify`
    - Updated `gpn` alias to use `git push --no-verify`
    - Introduced `glog` alias for a customized git log format with timestamp
    - Cleaned up commented-out alias for clarity

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -13,14 +13,19 @@ alias bsetup="ie && pnpm i"
 
 # Git command
 alias gc="git commit"
+alias gcn="git commit --no-verify"
 alias gcn="git commit -n"
 alias gs="git status"
 alias gp="git push && open raycast://extensions/raycast/raycast/confetti"
-alias gpn="git push -n"
+alias gpn="git push --no-verify && open raycast://extensions/raycast/raycast/confetti"
+# alias gpn="git push -n"
 alias gaa="git add --all"
 alias graph="git log --all --decorate --oneline --graph"
 alias gcg="git config --edit --global"
 alias gcl="git config --edit --local"
+# git log oneline with the timestamp
+alias glog="git log --decorate --date=iso --pretty=format:'%C(auto)%h%d %C(cyan)%ad%C(reset) %s' --color=always"
+#
 
 # Tmux
 alias tma="tmux a"
