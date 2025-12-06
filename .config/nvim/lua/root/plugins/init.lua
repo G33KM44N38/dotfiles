1ed6c958a616c3bb2ee10f9f1a73cad1b346b892
kylian <kylianmayanga@gmail.com>
2025-12-06 11:37:26 +0100

    feat(plugins): add git-blame.nvim plugin for Git blame functionality
    Added the git-blame.nvim plugin to enhance Git blame capabilities in Neovim. The plugin is configured to load lazily and includes options for customizing the blame message format and date format.
    
    - Configured lazy loading for performance
    - Set default options for message and date formats
    - Included virtual text column configuration for display purposes

--- .config/nvim/lua/root/plugins/init.lua
+++ .config/nvim/lua/root/plugins/init.lua
@@ -1,5 +1,22 @@
 return {
 	{
+		"f-person/git-blame.nvim",
+		-- load the plugin at startup
+		event = "VeryLazy",
+		-- Because of the keys part, you will be lazy loading this plugin.
+		-- The plugin will only load once one of the keys is used.
+		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
+		-- or lazy = false. One of both options will work.
+		opts = {
+			-- your configuration comes here
+			-- for example
+			enabled = false, -- if you want to enable the plugin
+			message_template = "<author> •  <summary> • <date> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		},
+	},
+	{
 		"echasnovski/mini.indentscope",
 		version = false,
 		config = function()
@@ -22,6 +39,7 @@ return {
 	"tpope/vim-repeat",
 	"tpope/vim-sensible",
 	"voldikss/vim-floaterm",
+	"nvim-treesitter/nvim-treesitter-context",
 	{
 		"fatih/vim-go",
 		keys = false,
