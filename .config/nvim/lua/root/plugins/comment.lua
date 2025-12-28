bd219e8e0c7d6a191ebf1f13e5cc756d8ccd8b5b
kylian <kylianmayanga@gmail.com>
2025-12-28 12:02:31 +0800

    fix(plugins): correct plugin name and add dependencies for commenting
    Corrected the plugin name from "numToSTr/Comment.nvim" to "numToStr/Comment.nvim" and added a dependency on "JoosepAlviste/nvim-ts-context-commentstring".
    
    - Fixed typo in the plugin name
    - Added dependencies to enhance comment functionality
    - Updated configuration to use a pre-hook for context-aware commenting

--- .config/nvim/lua/root/plugins/comment.lua
+++ .config/nvim/lua/root/plugins/comment.lua
@@ -1,5 +1,12 @@
 return {
-	"numToSTr/Comment.nvim",
+	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
 }
