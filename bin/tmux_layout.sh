82ebbadecd521db04ed3e2803dfa3a02916d41f3
kylian <kylianmayanga@gmail.com>
2026-02-11 15:45:50 +0100

    refactor(tmux): Simplify command execution in tmux scripts
    Refactored the tmux scripts to streamline command execution and improve readability. Removed unnecessary conditional checks and consolidated command sequences.
    
    - Updated commands in `coding-assistant` to remove unnecessary flags
    - Simplified logic in `tmux_layout.sh` for clearer window management
    - Enhanced overall maintainability of the scripting logic

--- bin/tmux_layout.sh
+++ bin/tmux_layout.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# Absolute path to skip
 WORKSPACE_PATH="/Users/boss/Library/Mobile Documents/iCloud~md~obsidian/Documents/Second_Brain"
 DATABASE_PATH="/Users/boss/coding/work/database"
 
-# Get current directory full path
 CURRENT_DIR="$PWD"
 SPECIFIC_COMMAND="nvim ."
-should_exclude=false
 
 case "$CURRENT_DIR" in
   "$WORKSPACE_PATH")
-    should_exclude=true
-    SPECIFIC_COMMAND="odn"
+    tmux rename-window 'nvim' \
+      \; send-keys -R "odn" C-m \
+      \; new-window -n 'opencode' \
+      \; send-keys -t 'opencode' -R "opencode" C-m \
+      \; select-window -t 1
     ;;
+    
   "$DATABASE_PATH")
-    should_exclude=true
-    SPECIFIC_COMMAND='vi -c ":DBUIToggle"'
+    tmux rename-window 'nvim' \
+      \; send-keys -R 'vi -c ":DBUIToggle"' C-m \
+      \; select-window -t 1
+    ;;
+    
+  *)
+    tmux rename-window 'nvim' \
+      \; send-keys -R "$SPECIFIC_COMMAND" C-m \
+      \; new-window -n 'run' \
+      \; new-window -n 'process' \
+      \; new-window -n 'assistant' \
+      \; send-keys -t 'assistant' -R "coding-assistant" C-m \
+      \; select-window -t 1
     ;;
 esac
-
-if [[ "$should_exclude" == true ]]; then
-  tmux rename-window 'nvim' \; send-keys -R "$SPECIFIC_COMMAND" C-m \; select-window -t 1
-else
-  tmux rename-window 'nvim' \
-    \; send-keys -R "$SPECIFIC_COMMAND" C-m \
-    \; new-window -n 'run' \
-    \; new-window -n 'process' \
-    \; new-window -n 'assistant' \
-    \; send-keys -t 'assistant' -R "coding-assistant" C-m \
-    \; select-window -t 1
-fi
