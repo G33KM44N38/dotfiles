f17a89665fa3619d95e0875aca2b6dc198045f63
kylian <kylianmayanga@gmail.com>
2026-05-22 08:58:05 +0200

    feat: add light theme modes

--- .config/nvim/lua/root/plugins/colorscheme.lua
+++ .config/nvim/lua/root/plugins/colorscheme.lua
@@ -1,30 +1,66 @@
 return {
-	"bluz71/vim-nightfly-guicolors",
-	priority = 1000,
-	config = function()
-		-- Lua initialization file
-		vim.g.nightflyTransparent = true
-		vim.g.nightflyVirtualTextColor = true
-		vim.cmd([[colorscheme nightfly]])
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 999,
+		opts = {
+			flavour = "latte",
+			transparent_background = true,
+			term_colors = true,
+		},
+	},
+	{
+		"bluz71/vim-nightfly-guicolors",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.nightflyTransparent = true
+			vim.g.nightflyVirtualTextColor = true
 
-		vim.api.nvim_set_hl(0, "LineNr", { fg = "white" })
+			local themes = {
+				dark = {
+					background = "dark",
+					colorscheme = "nightfly",
+					line_nr = "white",
+				},
+				light = {
+					background = "light",
+					colorscheme = "catppuccin-latte",
+					line_nr = "#5c5f77",
+				},
+			}
 
-		-- vim.o.background = "light"
-		-- vim.cmd('colorscheme gruvbox')
+			local function apply_theme(mode)
+				local theme = themes[mode] or themes.dark
+				vim.o.background = theme.background
+				vim.cmd.colorscheme(theme.colorscheme)
+				vim.api.nvim_set_hl(0, "LineNr", { fg = theme.line_nr })
+				vim.g.root_theme_mode = mode
+			end
 
-		-- vim.cmd('colorscheme solarized8_high')
-		-- vim.cmd('colorscheme gruvbox')
-		-- vim.cmd([[colorscheme alduin]])
-		-- vim.cmd([[colorscheme solarized8_high]])
-		-- vim.cmd('colorscheme iceberg')
-		-- vim.cmd([[colorscheme gotham]])
-		-- vim.cmd([[colorscheme papercolor]])
-		-- vim.cmd([[colorscheme mountaineer]])
-		-- vim.cmd([[colorscheme onedark]])
-		-- vim.cmd([[colorscheme sierra]])
-		-- vim.cmd([[colorscheme termschool]])
-		-- vim.cmd([[colorscheme molokai]])
-		-- vim.cmd([[colorscheme 256_noir]])
-		-- vim.cmd([[colorscheme atom]])
-	end,
+			local function preferred_theme_mode()
+				local env_mode = vim.env.NVIM_THEME or vim.env.THEME_MODE
+				if env_mode == "light" or env_mode == "dark" then
+					return env_mode
+				end
+
+				return "dark"
+			end
+
+			vim.api.nvim_create_user_command("ThemeLight", function()
+				apply_theme("light")
+			end, {})
+
+			vim.api.nvim_create_user_command("ThemeDark", function()
+				apply_theme("dark")
+			end, {})
+
+			vim.api.nvim_create_user_command("ThemeToggle", function()
+				apply_theme(vim.g.root_theme_mode == "light" and "dark" or "light")
+			end, {})
+
+			apply_theme(preferred_theme_mode())
+		end,
+	},
 }
